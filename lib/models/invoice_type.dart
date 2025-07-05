bd01a112300fc415e6e9604a960e1efad53c41b0
abdullah774363393 <oov82214@gmail.com>
2025-07-05 09:18:46 +0300

    Update invoice_type.dart

--- lib/models/invoice_type.dart
+++ lib/models/invoice_type.dart
@@ -7,7 +7,7 @@ part 'invoice_type.g.dart';
 // ⭐⭐ هام: تأكد أن typeId هذا فريد ولم تستخدمه لأي كلاس أو enum آخر
 // اختر رقمًا لا يتعارض مع typeId: 3 لـ Invoice أو أي typeId آخر لديك.
 // على سبيل المثال، إذا كانت لديك typeId 0, 1, 2, 3 استخدم 4 أو 20.
-@HiveType(typeId: 4) // مثال: استخدم رقمًا فريدًا
+@HiveType(typeId: 5) // مثال: استخدم رقمًا فريدًا
 enum InvoiceType {
   @HiveField(0)
   sale, // فاتورة مبيعات
