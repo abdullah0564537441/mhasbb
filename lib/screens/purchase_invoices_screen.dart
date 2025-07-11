b47c31f69dadf30c041574f8bf6f048d76b8f24c
abdullah0564537441 <abdullah0564537441@gmail.com>
2025-07-11 18:33:57 +0300

    Update purchase_invoices_screen.dart

--- lib/screens/purchase_invoices_screen.dart
+++ lib/screens/purchase_invoices_screen.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
 
 import 'package:mhasbb/models/invoice.dart';
+import 'package:mhasbb/models/invoice_type.dart'; // ⭐ أضف هذا السطر ⭐
 import 'package:mhasbb/screens/add_edit_purchase_invoice_screen.dart'; // تأكد من وجوده
 
 class PurchaseInvoicesScreen extends StatefulWidget {
