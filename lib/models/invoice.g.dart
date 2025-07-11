ef00d60a40b1b05256cca33a3d2463ca2789c065
abdullah774363393 <oov82214@gmail.com>
2025-07-11 00:25:01 +0000

    حفظ التعديلات المحلية قبل الدمج

--- lib/models/invoice.g.dart
+++ lib/models/invoice.g.dart
@@ -27,13 +27,14 @@ class InvoiceAdapter extends TypeAdapter<Invoice> {
       supplierId: fields[7] as String?,
       supplierName: fields[8] as String?,
       paymentMethod: fields[9] as PaymentMethod,
+      originalInvoiceId: fields[10] as String?,
     );
   }
 
   @override
   void write(BinaryWriter writer, Invoice obj) {
     writer
-      ..writeByte(10)
+      ..writeByte(11)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -53,7 +54,9 @@ class InvoiceAdapter extends TypeAdapter<Invoice> {
       ..writeByte(8)
       ..write(obj.supplierName)
       ..writeByte(9)
-      ..write(obj.paymentMethod);
+      ..write(obj.paymentMethod)
+      ..writeByte(10)
+      ..write(obj.originalInvoiceId);
   }
 
   @override
@@ -63,6 +66,55 @@ class InvoiceAdapter extends TypeAdapter<Invoice> {
   bool operator ==(Object other) =>
       identical(this, other) ||
       other is InvoiceAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class InvoiceTypeAdapter extends TypeAdapter<InvoiceType> {
+  @override
+  final int typeId = 1;
+
+  @override
+  InvoiceType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return InvoiceType.sale;
+      case 1:
+        return InvoiceType.purchase;
+      case 2:
+        return InvoiceType.salesReturn;
+      case 3:
+        return InvoiceType.purchaseReturn;
+      default:
+        return InvoiceType.sale;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, InvoiceType obj) {
+    switch (obj) {
+      case InvoiceType.sale:
+        writer.writeByte(0);
+        break;
+      case InvoiceType.purchase:
+        writer.writeByte(1);
+        break;
+      case InvoiceType.salesReturn:
+        writer.writeByte(2);
+        break;
+      case InvoiceType.purchaseReturn:
+        writer.writeByte(3);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is InvoiceTypeAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
