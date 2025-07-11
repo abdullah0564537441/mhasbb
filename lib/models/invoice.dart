b49100da145829c81e2f4bf5405a909c1af293d3
abdullah774363393 <oov82214@gmail.com>
2025-07-11 03:15:56 +0300

    Update invoice.dart

--- lib/models/invoice.dart
+++ lib/models/invoice.dart
@@ -1,12 +1,11 @@
 // lib/models/invoice.dart
 import 'package:hive/hive.dart';
 import 'package:mhasbb/models/invoice_item.dart';
-import 'package:mhasbb/models/invoice_type.dart';
-import 'package:mhasbb/models/payment_method.dart'; // ⭐ أضف هذا السطر
+import 'package:mhasbb/models/payment_method.dart'; // تأكد من استيرادها
 
 part 'invoice.g.dart';
 
-@HiveType(typeId: 3) // تأكد أن هذا الـ typeId فريد
+@HiveType(typeId: 3) // تأكد أن هذا الـ typeId فريد وغير مستخدم لنماذج أخرى
 class Invoice extends HiveObject {
   @HiveField(0)
   late String id;
@@ -15,7 +14,7 @@ class Invoice extends HiveObject {
   late String invoiceNumber;
 
   @HiveField(2)
-  late InvoiceType type;
+  late InvoiceType type; // نوع الفاتورة (بيع، شراء، مرتجع بيع، مرتجع شراء)
 
   @HiveField(3)
   late DateTime date;
@@ -35,8 +34,11 @@ class Invoice extends HiveObject {
   @HiveField(8)
   String? supplierName;
 
-  @HiveField(9) // ⭐ أضف هذا الحقل الجديد
+  @HiveField(9) // ⭐ حقل طريقة الدفع (نقد، آجل، تحويل بنكي)
   late PaymentMethod paymentMethod;
+
+  @HiveField(10) // ⭐ حقل جديد لربط المرتجع بالفاتورة الأصلية
+  String? originalInvoiceId;
 
   Invoice({
     required this.id,
@@ -48,6 +50,19 @@ class Invoice extends HiveObject {
     this.customerName,
     this.supplierId,
     this.supplierName,
-    this.paymentMethod = PaymentMethod.cash, // ⭐ قيمة افتراضية
+    this.paymentMethod = PaymentMethod.cash, // قيمة افتراضية لطريقة الدفع
+    this.originalInvoiceId, // تهيئة الحقل الجديد
   });
+}
+
+@HiveType(typeId: 1) // ⭐ تأكد أن هذا الـ typeId فريد لأنواع الفواتير
+enum InvoiceType {
+  @HiveField(0)
+  sale, // فاتورة مبيعات
+  @HiveField(1)
+  purchase, // فاتورة مشتريات
+  @HiveField(2) // ⭐ مرتجع مبيعات
+  salesReturn,
+  @HiveField(3) // ⭐ مرتجع مشتريات
+  purchaseReturn,
 }
